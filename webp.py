7d51d05ebd78630637d8a02b67245f00d0d84a0f
InstilKay <instilpee@gmail.com>
2025-01-10 16:39:53 +0000

    Update webp.py

--- webp.py
+++ webp.py
@@ -1,9 +1,6 @@
 
 import streamlit as st
 import pandas as pd
-import time
-import requests
-from bs4 import BeautifulSoup
 table1=pd.DataFrame({"RPA":[1,2,3,4,5,6,7],"Flows":[11,12,13,14,15,16,17]})
 st.title                ("AUTOMATION")
 st.subheader("Traning for developers")
